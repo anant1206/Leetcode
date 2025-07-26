bd177607ccf3b653c6c5b45c9cb4a2554e003ac0
anant1206 <95143090+anant1206@users.noreply.github.com>
2025-07-26 20:13:44 +0530

    [17] [Time Beats: 100.00%] [Memory Beats: 17.18%] - LeetPush

--- Letter-Combinations-of-a-Phone-Number.java
+++ Letter-Combinations-of-a-Phone-Number.java
@@ -1,10 +1,12 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        
         if (digits == null || digits.length() == 0) {
-            return new ArrayList<>();
+            return res;
         }
-
-        HashMap<Character, String> map = new HashMap<>();
+        
+        Map<Character, String> map = new HashMap<>();
         map.put('2', "abc");
         map.put('3', "def");
         map.put('4', "ghi");
@@ -13,22 +15,22 @@ class Solution {
         map.put('7', "pqrs");
         map.put('8', "tuv");
         map.put('9', "wxyz");
-
-        List<String> ans = new ArrayList<>();
-        helper(digits, 0, "", map, ans);
-        return ans;  
+        
+        helper(digits, 0, new StringBuilder(), res, map);    
+        return res;        
     }
 
-    private void helper(String digits, int index, String current, HashMap<Character, String> map, List<String> ans) {
-        if (index == digits.length()) {
-            ans.add(current);
+    private void helper(String digits, int i, StringBuilder current, List<String> res, Map<Character, String> map) {
+        if (i == digits.length()) {
+            res.add(current.toString());
             return;
         }
-
-        char digit = digits.charAt(index);
-        String letters = map.get(digit);
+        
+        String letters = map.get(digits.charAt(i));
         for (char letter : letters.toCharArray()) {
-            helper(digits, index + 1, current + letter, map, ans);
+            current.append(letter);
+            helper(digits, i + 1, current, res, map);
+            current.deleteCharAt(current.length() - 1); // backtrack
         }
-    }
+    }    
 }
