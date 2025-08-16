3c3f15437cbd2ee3bfcb4b8956f07fab47c69b67
anant1206 <95143090+anant1206@users.noreply.github.com>
2025-08-16 23:44:26 +0530

    [2221] [Time Beats: 29.14%] [Memory Beats: 41.87%] - LeetPush

---
+++ Find-Triangular-Sum-of-an-Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        while(nums.length > 1){
+            int[] next = new int[nums.length-1];
+            for(int i=0; i< next.length; i++){
+                next[i] = (nums[i] + nums[i+1]) % 10;
+            }
+            // copies temp array to nums
+            nums = next;
+        }
+        return nums[0];
+    }
+}
